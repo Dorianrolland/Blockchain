a7ad5001cbbb5d7c7594abd615ddc953013679db
Dorianrolland <124084062+Dorianrolland@users.noreply.github.com>
2026-03-18 16:25:50 +0100

    Add checkInWithPermit and nonces functions to interface

--- contracts/interfaces/ICheckInRegistry.sol
+++ contracts/interfaces/ICheckInRegistry.sol
@@ -3,4 +3,6 @@ pragma solidity ^0.8.28;
 
 interface ICheckInRegistry {
     function isUsed(uint256 tokenId) external view returns (bool);
+    function checkInWithPermit(uint256 tokenId, uint256 deadline, bytes calldata signature) external;
+    function nonces(uint256 tokenId) external view returns (uint256);
 }
